75723f5febab6a511c76af9d4cd4b1e4c6da9d0e
john dunne <johnjdunneone@yahoo.ie>
2021-01-09 15:34:00 +0000

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/buy-and-sell'));
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname+
+  '/dist/buy-and-sell/index.html'));
+});
+app.listen(precess.env.POST || 8080);
